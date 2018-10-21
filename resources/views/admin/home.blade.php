0c0f24b545e3d1858f6237ce08726ef94530d083
Your Name <you@example.com>
2018-10-21 19:27:21 +0300

    Updated

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Notes</div>
+                <div class="card-header">Admin Panel</div>
 
                 <div class="card-body">
                     @if (session('status'))
